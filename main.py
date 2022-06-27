08de933bce16e387cec6fe0e6658d046214898c6
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-06-27 16:20:14 +0300

    refactoring

--- main.py
+++ main.py
@@ -9,24 +9,34 @@ CORRECT_TITLES = ['ВТП Казани, млн руб.', 'Добавленная
                   'ДС Деятельность в области информатизации и связи, тыс. руб.']
 
 def is_titles_ok(data_frame_object):
-    titles = data_frame_object["Наименование"].to_list()
+    '''
+        >>> df = pandas.read_excel('XXXX')
+        >>> is_titles_ok(df)
+        ...
+    '''
+    # + doctest
+    titles = data_frame_object["Наименование"].to_list() # определить конкретно, какие параметры отсутствуют
+    
     if titles == CORRECT_TITLES:
         return True
     else:
         return False
 
 def is_empty(data_frame_object):
-
+    # + doctest
     foundEmpty = False
-    for i in [3, 2, 1]:
-        filling = data_frame_object[f"Y-{i}"].to_list()
+
+    for i in [3, 2, 1]: # использовать генератор списков
+        filling = data_frame_object[f"Y-{i}"].to_list() # заменить f"Y-{i}" на года
+
         for j in range(6):
-            if pandas.DataFrame(filling).isnull()[0][j] == True:
+            if pandas.DataFrame(filling).isnull()[0][j] == True: # yt
                 return True
+
     return False
 
 
-def check_table(file_name):
+def check_table(file_name): # переименовать + возвращать bool
     """
     >>> check_table("correct1.xlsx")
     'Everything is fine!'
