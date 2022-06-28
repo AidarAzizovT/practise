1d9e330657537d5da6d6e49931dfe1e4306c9024
Aidar <azizow37@gmail.com>
2022-06-28 09:35:29 +0300

    In the morning brain works really better, I think that I've fixed all mistakes, at least i hope so.

--- main.py
+++ main.py
@@ -10,58 +10,112 @@ CORRECT_TITLES = ['ВТП Казани, млн руб.', 'Добавленная
 
 def is_titles_ok(data_frame_object):
     '''
-        >>> df = pandas.read_excel('XXXX')
+     + doctest
+    определить конкретно, какие параметры отсутствуют
+
+        >>> df = pandas.read_excel('wrong1.xlsx')
         >>> is_titles_ok(df)
-        ...
+        С параметром на 7 строке ошибка
+        False
+
+        >>> df = pandas.read_excel('wrong3.xlsx')
+        >>> is_titles_ok(df)
+        С параметром на 4 строке ошибка
+        False
+
+        >>> df = pandas.read_excel('correct1.xlsx')
+        >>> is_titles_ok(df)
+        True
+
+         >>> df = pandas.read_excel('correct2.xlsx')
+        >>> is_titles_ok(df)
+        True
+
+
     '''
-    # + doctest
-    titles = data_frame_object["Наименование"].to_list() # определить конкретно, какие параметры отсутствуют
-    
-    if titles == CORRECT_TITLES:
+    foundMistake = False
+    counter = 2 #Параметры начинаются со 2 строчке в excel
+    for title in data_frame_object.values:
+        if title[0] not in CORRECT_TITLES:
+            print(f'С параметром на {counter} строке ошибка')
+            foundMistake = True
+        counter += 1
+
+    if foundMistake == False:
         return True
     else:
         return False
 
+
+
 def is_empty(data_frame_object):
     # + doctest
+    # просто пройтись по всем 3м столбцам начиная со второго
+    # заменить f"Y-{i}" на года
+
+    '''
+        >>> df = pandas.read_excel('wrong2.xlsx')
+        >>> is_empty(df)
+        Ошибка по  4 столбцу 4 строке
+        Ошибка по  3 столбцу 6 строке
+        Ошибка по  2 столбцу 7 строке
+        True
+
+        >>> df = pandas.read_excel('wrong3.xlsx')
+        >>> is_empty(df)
+        Ошибка по  4 столбцу 8 строке
+        True
+
+        >>> df = pandas.read_excel('correct1.xlsx')
+        >>> is_empty(df)
+        False
+    '''
+
     foundEmpty = False
+    line = 2
+    row = 2
 
-    for i in [3, 2, 1]: # просто пройтись по всем 3м столбцам начиная со второго
-        filling = data_frame_object[f"Y-{i}"].to_list() # заменить f"Y-{i}" на года
+    for string in data_frame_object.values:
+        for element in string[1:]:
+            if pandas.isna(element):
+                foundEmpty = True
+                print(f"Ошибка по  {str(row)} столбцу {str(line)} строке")
+            row += 1
+        row = 2
+        line += 1
 
-        for j in range(6):
-            if pandas.DataFrame(filling).isnull()[0][j] == True: # yt
-                return True
+    if foundEmpty == True:
+        return True
+    else:
+        return False
 
-    return False
 
 
-def check_table(file_name): # переименовать + возвращать bool
-    """
-    >>> check_table("correct1.xlsx")
-    'Everything is fine!'
 
-    >>> check_table("correct2.xlsx")
-    'Everything is fine!'
 
-    >>> check_table("wrong1.xlsx")
-    'Something is not correct'
 
-    >>> check_table("wrong2.xlsx")
-    'Something is not correct'
+def is_everything_ok(file_name): # переименовать + возвращать bool
+    '''
+        >>> is_everything_ok("correct1.xlsx")
+        True
 
-    >>> check_table("wrong3.xlsx")
-    'Something is not correct'
+        >>> is_everything_ok("wrong1.xlsx")
+        С параметром на 7 строке ошибка
+        False
 
-    """
+        >>> is_everything_ok("wrong2.xlsx")
+        Ошибка по  4 столбцу 4 строке
+        Ошибка по  3 столбцу 6 строке
+        Ошибка по  2 столбцу 7 строке
+        False
+
+    '''
+
     data_frame_object = pandas.read_excel(file_name)
     if is_titles_ok(data_frame_object) and not is_empty(data_frame_object):
-        return 'Everything is fine!'
+        return True
     else:
-        return 'Something is not correct'
-
-
-#print(check_table("wrong2.xlsx"))
+        return False
 
 
 
@@ -69,5 +123,8 @@ def check_table(file_name): # переименовать + возвращать 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+
+
+
 
 
